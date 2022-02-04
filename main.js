13708ffc7c9648d67a698acf33198c557bc43f12
Isaías Roberto <isaias.reng@gmail.com>
2022-02-04 18:57:04 -0300

    javascript

---
+++ main.js
@@ -0,0 +1,36 @@
+const contador = document.getElementById("contador");
+const soma = document.getElementById("mais__produto");
+const sub = document.getElementById("menos__produto");
+
+const b8 = document.getElementById("b8one")
+
+let numero = 0;
+
+soma.addEventListener("click", ()=> {
+
+    if(numero == 3){}
+
+    else{
+        numero++;
+        contador.innerHTML = numero;
+
+    }
+   
+});
+
+sub.addEventListener("click", ()=> {
+
+    if(numero == 0){}
+        else{
+
+            numero--;
+            contador.innerHTML = numero;
+
+        }
+    
+
+    
+});
+
+b8.addEventListener("al")
+
